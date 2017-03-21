3211a082d8121ca4461f48ba43003e74990c8429
Kohl <marcelokohl@gmail.com>
2017-03-21 10:17:59 -0300

    Validação ddos formulários de signin signup
    v1

---
+++ user/themes/photo-container-site/_assets/js/inputmask.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".email-mask").inputmask({
+    mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+    greedy: false,
+    onBeforePaste: function (pastedValue, opts) {
+      pastedValue = pastedValue.toLowerCase();
+      return pastedValue.replace("mailto:", "");
+    },
+    definitions: {
+      '*': {
+        validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+        cardinality: 1,
+        casing: "lower"
+      }
+    }
+  }); //static mask
+});
