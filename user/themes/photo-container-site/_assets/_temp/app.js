c0f2c155a4c95241e1a32317f8c77d39b6fbfd03
Kohl <marcelokohl@gmail.com>
2017-04-21 23:43:44 -0300

    [Novo] modal alert

--- user/themes/photo-container-site/_assets/_temp/app.js
+++ user/themes/photo-container-site/_assets/_temp/app.js
@@ -279,6 +279,22 @@ $(document).ready(function(){
   }); //static mask
 });
 
+$( document ).ready(function() {
+})
+
+function show_modal_alert(type, url, text) {
+  $('#modal-alert .modal-body').html(text);
+
+  $('#modal-alert #modal-alert-header').removeClass();
+  $('#modal-alert #modal-alert-header').addClass('modal-header alert alert-'+type);
+
+  $('#modal-alert #modal-alert-confirm').removeClass();
+  $('#modal-alert #modal-alert-confirm').addClass('btn btn-'+type);
+  $('#modal-alert #modal-alert-confirm').attr('href', url);
+  
+  $('#modal-alert').modal('show')
+}
+
 $(document).ready(function(){
   $(".form-validate.form-signin").validate({
     rules: {
