7a38e18f730697739771528962ee70425b0f1238
Kohl <marcelokohl@gmail.com>
2017-04-23 20:26:36 -0300

    ajustes modal...

--- user/themes/photo-container-site/_assets/js/modal.js
+++ user/themes/photo-container-site/_assets/js/modal.js
@@ -1,15 +1,20 @@
 $( document ).ready(function() {
 })
 
-function show_modal_alert(type, url, text) {
+function show_modal_alert(style, url, text) {
   $('#modal-alert .modal-body').html(text);
+  if(url === "") {
+    $('#modal-alert .modal-footer').hide();
+  }else {
+    $('#modal-alert .modal-footer').show();
+  }
 
   $('#modal-alert #modal-alert-header').removeClass();
-  $('#modal-alert #modal-alert-header').addClass('modal-header alert alert-'+type);
+  $('#modal-alert #modal-alert-header').addClass('modal-header alert alert-'+style);
 
   $('#modal-alert #modal-alert-confirm').removeClass();
-  $('#modal-alert #modal-alert-confirm').addClass('btn btn-'+type);
+  $('#modal-alert #modal-alert-confirm').addClass('btn btn-'+style);
   $('#modal-alert #modal-alert-confirm').attr('href', url);
-  
-  $('#modal-alert').modal('show')
+
+  $('#modal-alert').modal('show');
 }
