d71ae0b087ae1f425169fef1d9539638efd32454
Kohl <marcelokohl@gmail.com>
2017-04-24 22:13:44 -0300

    ajustes landing

--- user/themes/photo-container-site/_assets/_temp/app.js
+++ user/themes/photo-container-site/_assets/_temp/app.js
@@ -262,6 +262,12 @@ $(document).ready(function(){
   $(".cep-mask").inputmask({
     mask: "99.999-999",
   });
+
+	$(".phone-mask").inputmask({
+		mask: ["(99) 9999-9999", "(99) 99999-9999", ],
+		showMaskOnHover: false,
+		keepStatic: true
+	});
   $(".email-mask").inputmask({
     mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
     greedy: false,
