bd85612075bba5428220ec43968f44fc0e670e67
Kohl <marcelokohl@gmail.com>
2017-05-09 09:44:38 -0300

    Galeria e tradução da validação

--- user/themes/photo-container-site/_assets/_temp/app.js
+++ user/themes/photo-container-site/_assets/_temp/app.js
@@ -399,4 +399,29 @@ $(document).ready(function(){
       // alert("Liberou cadastro");
     }
   });
+
+
+
+  jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo é obrigatório.",
+    remote: "Por favor corrija este campo.",
+    email: "Insira um endereço de e-mail válido.",
+    url: "Insira uma URL válido.",
+    date: "Insira uma data válida.",
+    dateISO: "Insira uma data válida (ISO).",
+    number: "Insira um número válido.",
+    digits: "Insira apenas dígitos.",
+    creditcard: "Insira um número de cartão válido.",
+    equalTo: "Insira o mesmo valor novamente.",
+    accept: "Insira um valor com uma extensão válida;",
+    maxlength: jQuery.validator.format("Por favor, insira mais de {0} caracteres."),
+    minlength: jQuery.validator.format("Digite pelo menos {0} caracteres."),
+    rangelength: jQuery.validator.format("Digite um valor entre {0} e {1} caracteres longos."),
+    range: jQuery.validator.format("Digite um valor entre {0} e {1}."),
+    max: jQuery.validator.format("Digite um valor menor ou igual a {0}."),
+    min: jQuery.validator.format("Digite um valor maior ou igual a {0}.")
+  });
+
+
+
 });
